2ed6efd15c0b828fe968e82aacc79869b8d48fb0
fcangrand <fcangrand@gmail.com>
2020-02-11 12:49:10 +0100

    tri decroissant puis prend pizza tant qu'on peut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pizza import Pizza
 
-fname = "a_example.in"
+fname = "d_quite_big.in"
 x = 0
 
 # Partie Lecture fichier
@@ -8,6 +8,7 @@ with open("input/" + fname) as f :
    for line in f :
       if x == 0:
         maxSlices, pizzaTypes = line.split( )
+        maxSlices = int(maxSlices)
       elif x == 1:
         allPizza = []
         allPizzaSlice = line.split( )
@@ -17,15 +18,29 @@ with open("input/" + fname) as f :
 
 
 # Partie Algo...
-print('maxSlices', maxSlices)
-print('pizzaTypes', pizzaTypes)
-print('allPizza', allPizza)
+#print('maxSlices', maxSlices)
+#print('pizzaTypes', pizzaTypes)
+#print('allPizza', allPizza)
 
+# Tri tableau par pizaa avec le plus de parts
+allPizza.sort(key=lambda x: x.slices, reverse=True)
 
+#print('allPizza', allPizza)
+currentSlices = 0
+i = 0
+pizzaTaken = []
+while (i < len(allPizza)):
+  currentSlice = allPizza[i].slices
+  if (currentSlice + currentSlices <= maxSlices):
+    currentSlices = currentSlice + currentSlices
+    pizzaTaken.append(allPizza[i].index)
+  i += 1
+   
+#print(pizzaTaken)
 
 # Partie Ecriture Sortie
 fichier = open("output/" + fname, "w+")
-fichier.write(maxSlices)
-for pizz in allPizza:
-  fichier.write("\n"+str(pizz))
+fichier.write(str(len(pizzaTaken)) + "\n")
+for pizz in pizzaTaken:
+  fichier.write(str(pizz) + " ")
 fichier.close()
